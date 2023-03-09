16bc417f21caad2cebf9cfad2b3fe1337479ccc1
Nishant Kumar <nishant.kumar@dowjones.com>
2023-03-09 16:12:36 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import robot from "robotjs";
+
+const app = express();
+const port = 9999;
+
+app.get("/", (req, res) => {
+  // Get the screen size
+  const screenSize = robot.getScreenSize();
+
+  // Define the movement range
+  const xRange = screenSize.width;
+  const yRange = screenSize.height;
+
+  // Move the mouse cursor randomly every 5 seconds
+  setInterval(() => {
+    const randomX = Math.floor(Math.random() * xRange);
+    const randomY = Math.floor(Math.random() * yRange);
+    robot.moveMouse(randomX, randomY);
+  }, 5000);
+
+  res.send("Break Protocol Initiated!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
